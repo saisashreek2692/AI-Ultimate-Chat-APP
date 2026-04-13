08a91a0f4751d6c1bab53a34ecc02c3b2001f02f
Sai Sashreek Dittakavi <saisashreekd@gmail.com>
2026-04-13 17:11:06 +0530

    Updated with Redux provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router/dom";
 import router from "./Router.jsx";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </>
   );
 }
